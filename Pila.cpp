643c15db9bd82ca00b2517ee261179a69e3bfbe8
Javier Avila Lopez <contacto@javieravilalopez.es>
2025-03-17 18:51:00 +0100

    Modificaciones en Pila.cpp
    Constructor Pila(string name)
    
    Inicializa cima a nullptr para indicar que la pila está vacía.
    Asigna el nombre correctamente.
    nombrePila()
    
    Devuelve el nombre del poste correctamente.
    apilar(int num)
    
    Crea un nuevo nodo con el valor num y lo enlaza con la cima anterior.
    Muestra correctamente el mensaje en consola.
    desapilar()
    
    Verifica si la pila está vacía antes de desapilar.
    Elimina el nodo correctamente y devuelve el valor del disco eliminado.
    estaVacia()
    
    Comprueba si cima es nullptr para determinar si la pila está vacía

--- Pila.cpp
+++ Pila.cpp
@@ -1,30 +1,44 @@
 #include"Pila.h"
 
+// Constructor
 
-Pila::Pila(string name)
-{
-
-}
-
-std::string Pila::nombrePila()
-{
+Pila::Pila(string name) {
+  this->name=name;
+  this->cima = nullptr; // Inicializamos la pila vacia
 
 }
 
-void Pila::apilar(int num)
-{
-  cout<<"Apilando disco "<<num<<" en poste "<<name<<endl;
-}
-  
-int Pila::desapilar()
-{    
-
-  cout<<"Desapilando disco "<<num<<" del poste "<<name<<endl;
-
+// Devuelve el nombre de la pila
+std::string Pila::nombrePila() {
+  return name;
 }
 
-bool Pila::estaVacia()
-{
+// Apilar disco
+void Pila::apilar(int num) {
+  Nodo* nuevoNodo = new Nodo(num, cima);
+  cima = nuevoNodo; // Ahora la cima es el nuevo nodo
+  cout<<"Apilando disco "<< num <<" en poste "<<name<<endl;
+}
 
+
+// Desapilar un disco
+int Pila::desapilar(){
+
+  if(cima==nullptr){
+    cout<<"ERROR: Cima vacia"<<endl;
+    return -1;
+  }
+
+  int num = cima->valor;
+  Nodo* temp = cima;
+  cima = cima->siguiente; // Mover la cima al siguiente nodo
+  delete temp;
+  cout<<"Desapilando disco "<< num <<" del poste "<<name<<endl;
+  return num;
+}
+
+// Verificar si la pila está vacía
+bool Pila::estaVacia() {
+  return cima == nullptr;
 }
 
